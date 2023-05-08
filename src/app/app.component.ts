00ab101c88211ebe4037316e245a524bad6fce43
ChristianTongCruz <lx06657@innovacion.mx>
2023-05-08 15:32:25 -0500

    Angular Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CoopCultImport';
+  title = 'Cooperativa Cultural Import';
 }
---
+++ src/app/core/layout/core-public-layout/core-public-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core-public-layout',
+  templateUrl: './core-public-layout.component.html',
+  styleUrls: ['./core-public-layout.component.css']
+})
+export class CorePublicLayoutComponent {
+
+}
